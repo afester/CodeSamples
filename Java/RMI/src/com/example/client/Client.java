78819335ccd3fab1b7018756c887b10c94dfec8a
Andreas Fester <Andreas.Fester@gmx.de>
2013-03-14 11:16:55 +0100

    Added restrictive security policy

--- Java/RMI/src/com/example/client/Client.java
+++ Java/RMI/src/com/example/client/Client.java
@@ -1,11 +1,7 @@
 package com.example.client;
 
-import java.awt.event.ComponentEvent;
-import java.awt.event.ComponentListener;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
-
-import javax.swing.JFrame;
 
 import com.example.server.StringServer;
 
@@ -15,12 +11,13 @@ public class Client {
     * @param args
     */
    public static void main(String[] args) {
+	  // Set up security manager
 	  System.setProperty("java.security.policy", "clientsecurity.policy");
       if (System.getSecurityManager() == null) {
           System.setSecurityManager(new SecurityManager());
       }
-      System.err.println("Client is running ...");
 
+      System.err.println("Client is running ...");
       try {
     	  System.err.println("Retrieving Registry ...");
           Registry registry = LocateRegistry.getRegistry("localhost"); // args[0]);
