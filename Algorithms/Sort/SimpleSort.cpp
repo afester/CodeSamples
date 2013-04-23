19d21a7f21c953804d7e3dc913e3eb461cd415c1
Andreas Fester <Andreas.Fester@gmx.de>
2013-04-23 12:00:53 +0200

    Added first sort algorithms

---
+++ Algorithms/Sort/SimpleSort.cpp
@@ -0,0 +1,188 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <iterator>
+
+using namespace std;
+
+
+
+/**
+ * Dumps a vector of strings to an output stream.
+ */
+ostream& operator<<(ostream& stream, const vector<string>& data) {
+    stream << "[";
+
+    vector<string>::const_iterator iter = data.begin();
+    if (iter != data.end()) {
+        stream << '"' << *iter << '"';
+	iter++;
+    }
+
+    while(iter != data.end()) {
+        stream << ", \"" << *iter << '"';
+	iter++;
+    }
+
+    stream << "]";
+    return stream;
+}
+
+
+void saveList(const char* fileName, const vector<string>& data) {
+    ofstream ofs(fileName);
+    ofs << data.size();
+    ofs << "\n";	// need some sort of separator!
+    copy(data.begin(), data.end(),
+	 ostream_iterator<string>(ofs, "\n"));
+    ofs.close();
+}
+
+
+
+
+
+
+class SortAlgorithm {
+protected:
+    vector<string> unsortedArray;
+    vector<string> sortedArray;
+
+    vector<string> loadList(const char* fileName);
+
+    virtual void doSort() = 0;
+
+public:
+    SortAlgorithm();
+
+    virtual ~SortAlgorithm();
+
+    void generateData();
+
+    void run();
+};
+
+
+SortAlgorithm::SortAlgorithm() {
+    unsortedArray = loadList("array.dat");
+}
+
+
+SortAlgorithm::~SortAlgorithm() {
+}
+
+
+vector<string> SortAlgorithm::loadList(const char* fileName) {
+    vector<string> result;
+    ifstream ifs(fileName);
+    size_t size;
+    ifs >> size;
+    result.resize(size);
+    for (size_t i=0; i<size; i++) {
+       ifs >> result[i];
+    }
+    ifs.close();
+    return result;
+}
+
+
+void SortAlgorithm::generateData() {
+    vector<string> someList;
+    someList.push_back("Hello");
+    someList.push_back("World");
+    someList.push_back("Moon");
+    someList.push_back("Earth");
+    someList.push_back("Venus");
+    someList.push_back("Merkur");
+    someList.push_back("Mars");
+    someList.push_back("Saturn");
+    someList.push_back("Neptun");
+    someList.push_back("Uranus");
+    someList.push_back("Pluto");
+    someList.push_back("Asteroid");
+    someList.push_back("Sun");
+
+    saveList("array.dat", someList);
+}
+
+
+void SortAlgorithm::run() {
+    cerr << "BEFORE: " << unsortedArray << std::endl;
+    doSort();
+    cerr << "AFTER : " << sortedArray << std::endl;
+}
+
+
+class BraindeadSort : public SortAlgorithm {
+protected:
+    virtual void doSort();
+};
+
+
+/**
+ * Brain-dead sorting algorithm:
+ *  - Search the next value in the unsorted array
+ *  - push this value into the sorted array
+ *  - start from scratch
+ *
+ *  - Time complexity: O(n^2)
+ *  - Space complexity: O(2n)
+ */
+void BraindeadSort::doSort() {
+    string prev;
+    for (int i = 0;  i < unsortedArray.size();  i++) {
+
+	// Find the smallest element which is still larger than the previous element
+	string current;
+	for (int idx = 0;  idx < unsortedArray.size();  idx++) {
+	    if (unsortedArray[idx] > prev && (current == "" || unsortedArray[idx] < current)) {
+		current = unsortedArray[idx];
+	    }
+        }
+        sortedArray.push_back(current);
+
+        prev = current;
+    }
+}
+
+
+class SimpleSort : public SortAlgorithm {
+protected:
+    virtual void doSort();
+};
+
+
+/**
+ * SimpleSort sorting algorithm (in-place sorting):
+ *  - Search the next value in the unsorted array
+ *  - exchange values in the unsorted array
+ *  - start from scratch
+ *
+ *  - Time complexity: O(n^2)
+ *  - Space complexity: O(2n)
+ */
+void SimpleSort::doSort() {
+    for (int i = 0;  i < unsortedArray.size();  i++) {
+	for (int idx = 0;  idx < unsortedArray.size();  idx++) {
+	    if (unsortedArray[idx] > unsortedArray[i]) {
+		string temp = unsortedArray[idx];
+		unsortedArray[idx] = unsortedArray[i];
+		unsortedArray[i] = temp;
+	    }
+        }
+    }
+
+    sortedArray = unsortedArray;
+}
+
+
+
+int main() {
+    BraindeadSort sort1;
+    sort1.run();
+
+    SimpleSort sort2;
+    sort2.run();
+
+    return 0;
+}
