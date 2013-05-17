70284782d3fa4b9f3b340516d40a4f6322fe97f1
Andreas Fester <Andreas.Fester@gmx.de>
2013-05-17 13:12:08 +0200

    Added servlet sample project

---
+++ J2EE/ServletSample/src/com/example/ServletTest.java
@@ -0,0 +1,42 @@
+package com.example;
+
+import java.io.IOException;
+import java.io.PrintStream;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ServletTest
+ */
+@WebServlet("/ServletTest")
+public class ServletTest extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ServletTest() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+      PrintStream out = new PrintStream(response.getOutputStream());
+      out.println("Hello GET " + getServletConfig().getServletContext().getServerInfo());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+      PrintStream out = new PrintStream(response.getOutputStream());
+      out.println("Hello POST " + getServletConfig().getServletContext().getServerInfo());
+	}
+
+}
