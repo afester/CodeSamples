05a9ae7dacda8519bbcdccfabf1ee2ec6f189412
Andreas Fester <Andreas.Fester@gmx.de>
2014-03-21 10:34:25 +0100

    Added JASS project, renamed Security project to JCE

---
+++ Java/JAAS/src/com/example/JAASExample.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class JAASExample {
+   public void run() {
+      
+   }
+   
+
+   public static void main(String[] args) {
+      new JAASExample().run();
+   }
+}
