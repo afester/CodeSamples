db535d712ee83bb2d3a3509eb55f18f6d1b2eea3
Andreas Fester <Andreas.Fester@gmx.de>
2014-04-24 08:50:52 +0200

    Adjusted classpath

--- Algorithms/Parser/AntlrSample/antenv.sh
+++ Algorithms/Parser/AntlrSample/antenv.sh
@@ -1,5 +1,5 @@
 export USR_BIN="/c/Program Files"
-export JAVA_HOME="${USR_BIN}/Java/jdk1.7.0_25"
+export JAVA_HOME="${USR_BIN}/Java/jdk1.7.0_51"
 export ANT_HOME="${USR_BIN}/apache-ant-1.9.1"
 
 export PATH=${PATH}:${ANT_HOME}/bin:${JAVA_HOME}/bin
