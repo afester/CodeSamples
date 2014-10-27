16d667a0daa841a478e43bc5c3eda4161113609b
Andreas Fester <Andreas.Fester@gmx.de>
2014-10-27 12:32:13 +0100

    Added sample PIC code

--- Embedded/PIC/Simple16F74.X/main.asm
+++ Embedded/PIC/Simple16F74.X/main.asm
@@ -17,6 +17,19 @@ Reset:
         retfie
 
 Main:
-        goto    Main
+        clrf    PORTA
+
+        banksel ADCON1               ; bank 1 - ADCON1 and TRISA registers
+        movlw   0x06                 ; all PORTA pins are
+        movwf   ADCON1               ; digital
+        movlw   B'00111100'          ; RA0, RA1 are outputs
+        movwf   TRISA
+
+        banksel PORTA               ; bank 0 - PORTA register
+
+Loop:
+        bsf     PORTA, 1
+        bcf     PORTA, 1
+        goto    Loop
 
         END
