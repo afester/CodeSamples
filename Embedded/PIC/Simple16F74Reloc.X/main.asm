ec85933f942e17f791d69d4683819b2b0ffe1b74
Andreas Fester <Andreas.Fester@gmx.de>
2014-10-28 12:31:15 +0100

    Added serial procedures

--- Embedded/PIC/Simple16F74Reloc.X/main.asm
+++ Embedded/PIC/Simple16F74Reloc.X/main.asm
@@ -1,39 +1,53 @@
-        PROCESSOR 16f74
-        INCLUDE   p16f74.inc
+            PROCESSOR   16f74
+            INCLUDE     p16f74.inc
+            INCLUDE     table.inc
+
+            udata_shr                   ; NOTE: This should be udata - verify the map file!
+            global   Reg1, Reg2
+Reg1        res 1
+Reg2        res 1
 
 
-        udata_shr                   ; NOTE: This should be udata - verify the map file!
-        global   Reg1, Reg2
-Reg1    res 1
-Reg2    res 1
-
-
-        code
-        global  Main
-        extern  SerialInit, hexToDigits
+            code
+            global  Main
+            extern  SerialInit, SerialSendChar, SerialSendConstString, SerialSendHex, hexToDigits
 Main:
-        clrf    PORTA
+            clrf    PORTA
 
-        clrf    Reg1
+            clrf    Reg1
 
-        banksel ADCON1               ; bank 1 - ADCON1 and TRISA registers
-        movlw   0x06                 ; all PORTA pins are
-        movwf   ADCON1               ; digital
-        movlw   B'00111100'          ; RA0, RA1 are outputs
-        movwf   TRISA
+            banksel ADCON1               ; bank 1 - ADCON1 and TRISA registers
+            movlw   0x06                 ; all PORTA pins are
+            movwf   ADCON1               ; digital
+            movlw   B'00111100'          ; RA0, RA1 are outputs
+            movwf   TRISA
 
-        banksel PORTA               ; bank 0 - PORTA register
+            banksel PORTA               ; bank 0 - PORTA register
 
-        call    SerialInit
+            call    SerialInit
 
+            TBLINIT startMsg
+            call    SerialSendConstString
 
-        movlw   0x08
-        movwf   Reg2
-        call    hexToDigits
+            clrf    Reg1
 
-Loop:
-        bsf     PORTA, 1
-        bcf     PORTA, 1
-        goto    Loop
+CountLoop:
+            call    SerialSendHex           ; send value in Reg1 as hex digits
+            movlw   '\n'
+            call    SerialSendChar
 
-        END
+            incf    Reg1, F
+            movlw   0x0f
+            xorwf   Reg1, W
+            bnz     CountLoop
+
+            TBLINIT doneMsg
+            call    SerialSendConstString
+
+Endless:
+            goto    Endless
+
+startMsg:   de      "\nRunning...\n\0"
+doneMsg:    de      "Done.\0"
+
+            end
--- Embedded/PIC/Simple16F74Reloc.X/serial.asm
+++ Embedded/PIC/Simple16F74Reloc.X/serial.asm
@@ -1,26 +1,113 @@
-        PROCESSOR 16f74
-        INCLUDE   p16f74.inc
+            PROCESSOR 16f74
+            INCLUDE p16f74.inc
+            INCLUDE table.inc
 
-        code
-        global  SerialInit
+            code
 
+            extern  Reg1
+
+
+; Initializes the serial UART interface.
+;
+            global  SerialInit
 SerialInit:
 ; Initialize Serial I/O
-        BANKSEL      SPBRG           ; Bank 1
+            BANKSEL SPBRG               ; Bank 1
 
-;       MOVLW   .7                   ; initialize baud rate generator (19200 bps)
-        MOVLW   .10                  ; initialize baud rate generator (57600 bps)
-        MOVWF   SPBRG
+;            MOVLW   .7                     ; initialize baud rate generator (19200 bps)
+            MOVLW   .10                 ; initialize baud rate generator (57600 bps)
+            MOVWF   SPBRG
 
-        MOVLW   B'00100000'          ; initialize serial port transmit con
-        MOVWF   TXSTA
-        BSF     TXSTA, BRGH
+            MOVLW   B'00100000'         ; initialize serial port transmit con
+            MOVWF   TXSTA
+            BSF     TXSTA, BRGH
 
-        BANKSEL RCSTA                ; Bank 0
+            BANKSEL RCSTA               ; Bank 0
 
-        MOVLW   B'10010000'          ; initialize serial port receive cont
-        MOVWF   RCSTA
+            MOVLW   B'10010000'         ; initialize serial port receive cont
+            MOVWF   RCSTA
 
-        return
+            return
 
-        end
+
+; Sends a single character to the serial interface.
+;
+; @param W the character to transmit.
+            global  SerialSendChar
+SerialSendChar:
+; wait for transmit buffer ready
+            BANKSEL TXSTA               ; Bank 1
+loop:
+            btfss   TXSTA, TRMT
+            goto    loop
+
+; send next character
+            BANKSEL TXREG               ; Bank 0
+            MOVWF   TXREG
+
+            return
+
+
+; Sends a constant, null terminated string from the ROM to the serial interface.
+;
+; @param PMADDRH/PMADDR The code memory address of the string to send.
+            global  SerialSendConstString
+SerialSendConstString:
+            TBLRD
+            btfsc   STATUS, Z
+            return
+
+            call    SerialSendChar
+
+            TBLINC
+            goto    SerialSendConstString
+
+
+; Sends a two-digit hex value to the serial interface.
+;
+; @param Reg1   The value to send as two-digit hex string
+;
+            global  SerialSendHex
+SerialSendHex:
+
+; send high digit
+            TBLINIT hexChars            ; initialize table pointer
+
+            swapf   Reg1, W             ; load high nibble from Parameter
+            andlw   0x0f
+            TBLIDXW                         ; add offset to table pointer
+
+            TBLRD                           ; read value from table
+            call    SerialSendChar
+
+; send low digit
+            TBLINIT hexChars            ; initialize table pointer
+
+            movf    Reg1, W                 ; load value from parameter
+            andlw   0x0f                    ; low nibble
+            TBLIDXW                         ; add offset to table pointer
+
+            TBLRD                           ; read value from table
+            call    SerialSendChar
+
+            return
+
+hexChars:
+            de      '0'
+            de      '1'
+            de      '2'
+            de      '3'
+            de      '4'
+            de      '5'
+            de      '6'
+            de      '7'
+            de      '8'
+            de      '9'
+            de      'a'
+            de      'b'
+            de      'c'
+            de      'd'
+            de      'e'
+            de      'f'
+
+            end
