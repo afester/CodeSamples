04404b4eefbbfcfb4a86ec83f97e618c542ed459
Andreas Fester <Andreas.Fester@gmx.de>
2014-11-11 15:01:37 +0100

    Added generic Graph canvas

---
+++ Java/JavaFXSample/src/com/example/graph/GraphCanvas.java
@@ -0,0 +1,156 @@
+package com.example.graph;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.example.Graph;
+
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+
+public class GraphCanvas extends Canvas {
+
+    private GraphParameters gp;
+    private GraphicsContext context;
+    private List<Graph> graphs = new ArrayList<>();
+
+    public GraphCanvas(GraphParameters gp) {
+        super(gp.canvasWidth, gp.canvasHeight);
+        this.gp = gp;
+        context = getGraphicsContext2D();
+    }
+
+    private void drawGraph(Graph theGraph) {
+        // do not draw the graph if it is in an error state
+        if (theGraph.isError()) {
+           return;
+        }
+
+        context.beginPath();
+        context.setStroke(theGraph.getColor());
+        context.setLineWidth(1);
+        // FGV.context.setLineDash([]);
+
+        boolean move = true;
+       // theGraph.setT(this.t);
+        double scalePerPixel = 1/gp.getScale();
+        for (double x = gp.getFromX(); x < gp.getToX(); x += scalePerPixel) {
+           try {
+              Double y = theGraph.evaluate(x);
+
+              if (!Double.isNaN(y)) {
+
+                  double xpos = gp.toViewX(x);
+                  double ypos = gp.toViewY(y);
+                  System.err.println("y=" + y + ", ypos=" + ypos);
+                 if (move) {
+                    move = false;
+                    context.moveTo(xpos, ypos);
+                 } else {
+                    context.lineTo(xpos, ypos);
+                 }
+              } else {
+                 move = true;  // next non-NaN needs to MOVE first
+              }
+           } catch (Exception e) {
+            //  legendTable.setError(theGraph.getIndex(), e.getLocalizedMessage());
+              theGraph.setIsError(true);
+              return;
+           }
+        }
+
+        context.stroke();
+     }
+
+    
+    public void renderScene() {
+        context.clearRect(0, 0, getWidth(), getHeight());
+    
+        // draw the axes
+        drawAxis();
+        
+        // draw the graphs
+        for (Graph graph : graphs) {
+           drawGraph(graph);
+        }
+    }
+
+    
+    /**
+     * @param y tick position in model coordinates
+     */
+    private void drawYTick(double y) {
+        double ypos = gp.toViewY(y);
+
+        if (ypos > 20) { // do not draw a tick nearby the end of the axis!
+           context.moveTo(gp.getX0() - 5, ypos + 0.5);
+           context.lineTo(gp.getX0() + 5, ypos + 0.5);
+
+           String value = "" + Math.round(y); //  * 10) / 10.0;
+           double textWidth = 15; // / context.measureText(value).width;
+           context.fillText(value, gp.getX0() - textWidth - 7, ypos + 3);
+        }
+     }
+
+
+     /**
+      * @param x tick position in model coordinates
+      */
+     private void drawXTick(double x) {
+        double xpos = gp.toViewX(x);
+
+        context.moveTo(xpos + 0.5, gp.getY0() - 5);
+        context.lineTo(xpos + 0.5, gp.getY0() + 5);
+
+        String value = "" + Math.round(x); //  * 10) / 10.0;
+        double textWidth = 15; // / FGV.context.measureText(value).width;
+        context.fillText(value, xpos - textWidth / 2, gp.getY0() + 15);
+     }
+
+     /**
+      * Draws the axes including the ticks.
+      */
+     private void drawAxis() {
+        // set the axis visual properties
+        context.beginPath();
+        context.setStroke(Color.BLACK);
+        context.setLineWidth(1);
+        // FGV.context.setLineDash([]);
+
+        // draw the axis lines
+
+        // Y axis
+        context.moveTo(gp.getX0() + 0.5, getHeight() + 0.5);
+        context.lineTo(gp.getX0() + 0.5, 0.5);
+
+        // X axis
+        context.moveTo(gp.getLeftMargin() + 0.5, gp.getY0() + 0.5);
+        context.lineTo(getWidth() + 0.5,         gp.getY0() + 0.5);
+
+        double tick = gp.getUnit();
+
+        // draw the y scale
+        for (double y = -tick; y > gp.getFromY(); y -= tick) { // negative axis
+           drawYTick(y);
+        }
+        for (double y = tick; y < gp.getToY(); y += tick) { // positive axis
+           drawYTick(y);
+        }
+
+        // draw the x scale
+        for (double x = -tick; x > gp.getFromX(); x -= tick) { // negative axis
+           drawXTick(x);
+        }
+        for (double x = tick; x < gp.getToX(); x += tick) { // positive axis
+           drawXTick(x);
+        }
+
+        context.stroke();
+     }
+
+    public void addGraph(Graph graph) {
+        this.graphs.add(graph);
+    }
+
+}
---
+++ Java/JavaFXSample/src/com/example/graph/test/GraphParametersTest.java
@@ -0,0 +1,24 @@
+package com.example.graph.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.example.graph.GraphParameters;
+
+public class GraphParametersTest {
+
+    @Test
+    public void testScaling() {
+        GraphParameters gp = new GraphParameters(640, 480, 20, 20, 20, 20,
+                                                 -50.0, 200.0, 
+                                                 0.75, 40);
+        assertEquals(128, gp.getX0());
+        assertEquals(360, gp.getY0());
+        assertEquals(2.56, gp.getScale(), 0.01);
+        assertEquals(gp.getX0(), gp.toViewX(0));
+        assertEquals(gp.getY0(), gp.toViewY(0));
+        assertEquals(51, gp.getTickPixels());
+        assertEquals(20.0, gp.getUnit(), 0.01);
+    }
+}
