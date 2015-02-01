30606d13e3675ffebb45dac331feab6b33e9fb0e
Andreas Fester <Andreas.Fester@gmx.de>
2015-02-01 12:36:50 +0100

    Added libusb1 python sample

---
+++ Python/PyUdev/usblib.py
@@ -0,0 +1,15 @@
+'''
+Created on Jan 21, 2015
+
+@author: andreas
+'''
+
+import usb1
+
+def main():
+    context = usb1.USBContext()
+    for device in context.getDeviceList(skip_on_error=True):
+        print 'ID %04x:%04x' % (device.getVendorID(), device.getProductID()), '->'.join(str(x) for x in ['Bus %03i' % (device.getBusNumber(), )] + device.getPortNumberList()), 'Device', device.getDeviceAddress()
+
+if __name__ == '__main__':
+    main()
