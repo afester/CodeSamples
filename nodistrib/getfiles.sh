159649c3afa50d8644756f34530ad515ea5cb771
Andreas Fester <Andreas.Fester@gmx.de>
2015-05-07 22:23:28 +0200

    Made JCE sample working again

--- nodistrib/getfiles.sh
+++ nodistrib/getfiles.sh
@@ -5,3 +5,8 @@ wget https://github.com/antlr/website-antlr4/raw/gh-pages/download/antlr-4.1-com
 wget http://download.forge.ow2.org/asm/asm-4.1-bin.zip
 unzip -j asm-4.1-bin.zip asm-4.1/lib/all/asm-all-4.1.jar 
 rm asm-4.1-bin.zip
+
+wget https://www.bouncycastle.org/download/bcprov-jdk15on-152.jar
+
+wget http://sourceforge.net/projects/codec/files/codec/CoDec-build21/CoDec-build21-jdk13.jar
+
