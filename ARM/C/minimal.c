d72f1c9ce44a0e97cd49c63cb941fe35489df81d
Andreas Fester <Andreas.Fester@gmx.de>
2015-05-13 13:58:17 +0200

    Added some code snippets

---
+++ ARM/C/minimal.c
@@ -0,0 +1,9 @@
+int main() {
+  int a = 2;
+  int b = 4;
+  int c = 0;
+
+  c = a + b;
+
+  return c;
+}
---
+++ C/bare/main.c
@@ -0,0 +1,15 @@
+#include "add.h"
+
+static const char* value = "Hello World";
+
+void _start() {
+   main();
+}
+
+
+int main() {
+  int result = add(1, 3);
+ // char* p = value;
+ // while(*p) p++;
+  return 0;
+}
