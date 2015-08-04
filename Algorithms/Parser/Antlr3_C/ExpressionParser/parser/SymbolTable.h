cad515e459674cd56d2a856beacd48915716d32b
Andreas Fester <Andreas.Fester@gmx.de>
2015-08-04 14:58:05 +0200

    Initial version of C++ expression parser

---
+++ Algorithms/Parser/Antlr3_C/ExpressionParser/parser/SymbolTable.h
@@ -0,0 +1,62 @@
+
+#ifndef __SYMBOL_TABLE_H__
+#define __SYMBOL_TABLE_H__
+
+#include <map>
+
+
+typedef double (*MATH_FUN)(double arg);
+typedef enum {SYM_VAR, SYM_CONST, SYM_FUN} SYMBOL_KIND;
+
+class SymbolTableEntry {
+	std::string id;
+	SYMBOL_KIND kind;
+
+	union {
+		MATH_FUN fun;
+		double val;
+	};
+
+	friend class SymbolTable;
+
+public:
+	SymbolTableEntry();
+
+	// variable
+	SymbolTableEntry(const std::string& id);
+
+	// constant
+	SymbolTableEntry(const std::string& id, double value);
+
+	// f(x)
+	SymbolTableEntry(const std::string& id, MATH_FUN fun);
+
+	virtual ~SymbolTableEntry();
+
+	std::string kindToName(SYMBOL_KIND kind) const;
+
+	std::string toString() const;
+};
+
+
+
+class SymbolTable {
+	std::map<std::string, SymbolTableEntry> table;
+
+public:
+	SymbolTable();
+
+	virtual ~SymbolTable();
+
+	void init();
+
+	void dump();
+
+	void setValue(const std::string& id, double value);
+
+	double getValue(const std::string& id) const;
+
+	double executeFunction(const std::string& id, double argument) const;
+};
+
+#endif
