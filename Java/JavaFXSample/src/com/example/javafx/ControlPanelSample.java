ca0e2393db455c80a51db255add97669a7d70a80
Andreas Fester <Andreas.Fester@gmx.de>
2015-10-09 15:02:12 +0200

    Working on JavaFX sample

--- Java/JavaFXSample/src/com/example/javafx/ControlPanelSample.java
+++ Java/JavaFXSample/src/com/example/javafx/ControlPanelSample.java
@@ -33,16 +33,90 @@ public class ControlPanelSample {
        //  s7Panel.getTransforms().add(new Scale(0.6, 0.6));
         s7Panel.setValue(0);
 
-        BorderImage img1 = 
-                new BorderImage(new Image("/left.png"), new BorderWidths(6), new Insets(0),  
-                                new BorderWidths(5), true, BorderRepeat.REPEAT, BorderRepeat.REPEAT);
-        BorderImage img2 = 
-                new BorderImage(new Image("/top.png"), new BorderWidths(6), new Insets(5),  
-                                new BorderWidths(15), true, BorderRepeat.REPEAT, BorderRepeat.REPEAT);
-        BorderStroke bs3 = new BorderStroke(new Color(180/255f, 180/255f, 180/255f, 1.0), BorderStrokeStyle.SOLID, CornerRadii.EMPTY, new BorderWidths(2));
-        BorderStroke bs2 = new BorderStroke(new Color(190/255f, 190/255f, 190/255f, 1.0), BorderStrokeStyle.SOLID, CornerRadii.EMPTY, new BorderWidths(4));
-        BorderStroke bs1 = new BorderStroke(new Color(200/255f, 200/255f, 200/255f, 1.0), BorderStrokeStyle.SOLID, CornerRadii.EMPTY, new BorderWidths(6));
-        Border b = new Border(img1, img2, img1, img2, img1, img2); // bs1, bs2, bs3);
+        // Note: the single argument BorderWidths() constructor applies the given value to all edges!
+        // Note: all images are drawn on top of each other.
+        //       The border widths define how much from each edge of the image is to be drawn.
+        //       If the image is smaller than the required area, it is tiled/stretched according to the BorderRepeat parameters.
+        //       The insets define the distance between the border image and the edge of the region
+        //       The slices somehow define the piece of the image which shall be drawn (?????) and they correlate with the border widths
+        BorderImage topBorder = 
+                new BorderImage(new Image("/top.png"),         // the image 
+                                new BorderWidths(6, 0, 0, 0),  // the border width (top, right, bottom, left)
+                                new Insets(0, 6, 0, 6),        // the insets (distance from the region border) (top, right, bottom, left) 
+                                new BorderWidths(6, 0, 0, 0),  // the "slice" (top, right, bottom, left)
+                                false, 
+                                BorderRepeat.REPEAT, 
+                                BorderRepeat.REPEAT);
+
+        BorderImage topRightBorder = 
+                new BorderImage(new Image("/topright.png"),    // the image 
+                                new BorderWidths(6, 6, 0, 0),  // the border width (top, right, bottom, left)
+                                new Insets(0, 0, 0, 0),        // the insets (distance from the region border) (top, right, bottom, left) 
+                                new BorderWidths(6, 6, 0, 0),  // the "slice" (top, right, bottom, left)
+                                false, 
+                                BorderRepeat.REPEAT, 
+                                BorderRepeat.REPEAT);
+
+        BorderImage rightBorder = 
+                new BorderImage(new Image("/right.png"),       // the image 
+                                new BorderWidths(0, 6, 0, 0),  // the border width (top, right, bottom, left)
+                                new Insets(6, 0, 6, 0),        // the insets (distance from the region border) (top, right, bottom, left) 
+                                new BorderWidths(0, 6, 0, 0),  // the "slice" (top, right, bottom, left)
+                                false, 
+                                BorderRepeat.REPEAT, 
+                                BorderRepeat.REPEAT);
+
+        BorderImage bottomRightBorder = 
+                new BorderImage(new Image("/bottomright.png"), // the image 
+                                new BorderWidths(0, 6, 6, 0),  // the border width (top, right, bottom, left)
+                                new Insets(0, 0, 0, 0),        // the insets (distance from the region border) (top, right, bottom, left) 
+                                new BorderWidths(0, 6, 6, 0),  // the "slice" (top, right, bottom, left)
+                                false, 
+                                BorderRepeat.REPEAT, 
+                                BorderRepeat.REPEAT);
+
+        BorderImage bottomBorder = 
+                new BorderImage(new Image("/bottom.png"),      // the image 
+                                new BorderWidths(0, 0, 6, 0),  // the border width (top, right, bottom, left)
+                                new Insets(0, 6, 0, 6),        // the insets (distance from the region border) (top, right, bottom, left) 
+                                new BorderWidths(0, 0, 6, 0),  // the "slice" (top, right, bottom, left)
+                                false, 
+                                BorderRepeat.REPEAT, 
+                                BorderRepeat.REPEAT);
+
+        BorderImage bottomLeftBorder = 
+                new BorderImage(new Image("/bottomleft.png"),         // the image 
+                                new BorderWidths(0, 0, 6, 6),  // the border width (top, right, bottom, left)
+                                new Insets(0, 0, 0, 0),         // the insets (distance from the region border) (top, right, bottom, left) 
+                                new BorderWidths(0, 0, 6, 6),  // the "slice" (top, right, bottom, left)
+                                false, 
+                                BorderRepeat.REPEAT, 
+                                BorderRepeat.REPEAT);
+
+/****************************/
+        BorderImage leftBorder = 
+                new BorderImage(new Image("/left.png"),         // the image 
+                                new BorderWidths(0, 0, 0, 6),  // the border width (top, right, bottom, left)
+                                new Insets(6, 0, 6, 0),         // the insets (distance from the region border) (top, right, bottom, left) 
+                                new BorderWidths(0, 0, 0, 6),  // the "slice" (top, right, bottom, left)
+                                false,
+                                BorderRepeat.REPEAT, 
+                                BorderRepeat.REPEAT);
+
+        BorderImage topLeftBorder = 
+                new BorderImage(new Image("/topleft.png"),         // the image 
+                                new BorderWidths(6, 0, 0, 6),  // the border width (top, right, bottom, left)
+                                new Insets(0, 0, 0, 0),         // the insets (distance from the region border) (top, right, bottom, left) 
+                                new BorderWidths(6, 0, 0, 6),  // the "slice" (top, right, bottom, left)
+                                false, 
+                                BorderRepeat.REPEAT, 
+                                BorderRepeat.REPEAT);
+/****************************/
+
+        //BorderStroke bs3 = new BorderStroke(new Color(180/255f, 180/255f, 180/255f, 1.0), BorderStrokeStyle.SOLID, CornerRadii.EMPTY, new BorderWidths(2));
+        //BorderStroke bs2 = new BorderStroke(new Color(190/255f, 190/255f, 190/255f, 1.0), BorderStrokeStyle.SOLID, CornerRadii.EMPTY, new BorderWidths(4));
+        //BorderStroke bs1 = new BorderStroke(new Color(200/255f, 200/255f, 200/255f, 1.0), BorderStrokeStyle.SOLID, CornerRadii.EMPTY, new BorderWidths(6));
+        Border b = new Border(topBorder, topRightBorder, rightBorder, bottomRightBorder, bottomBorder, bottomLeftBorder, leftBorder, topLeftBorder);
 
         // NOTE: Setting the border on the SevenSegmentPanel causes the border
         // to be scaled along with the panel!!!!
