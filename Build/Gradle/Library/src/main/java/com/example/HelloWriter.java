e82aacd3419fe03823a91f7fe07471e07fc63106
Andreas Fester <Andreas.Fester@gmx.de>
2016-03-04 14:16:54 +0100

    Added gradle example, added pending code examples

--- Build/Gradle/Library/src/main/java/com/example/HelloWriter.java
+++ Build/Gradle/Library/src/main/java/com/example/HelloWriter.java
@@ -1,7 +1,15 @@
 package com.example;
 
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
+
 public class HelloWriter {
+  private static final Logger logger = LogManager.getLogger();
+
   public void sayHello(String to) {
+    logger.info("Saying a greeting ...");
+
     System.err.printf("Hello %s\n", to);
   }
 }
