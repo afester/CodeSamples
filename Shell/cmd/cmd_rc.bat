3f5dd95b5aab49d4aa33d58430f743ccc1ca39f1
Andreas Fester <Andreas.Fester@gmx.de>
2016-10-06 12:53:48 +0200

    Added sed example, added git merge-file script

--- Shell/cmd/cmd_rc.bat
+++ Shell/cmd/cmd_rc.bat
@@ -19,8 +19,10 @@ SET CMAKE_HOME=c:\Program Files (x86)\cmake-3.3.0-win32-x86
 SET MSVC_HOME=D:\Microsoft Visual Studio 12.0\VC
 SET NASM_HOME=C:\Users\afester\nasm-2.10.07
 SET HG_HOME=D:\Program Files\TortoiseHg
+SET GIT_HOME=D:\Program Files\Git
+SET HASKELL_HOME=D:\Program Files\Haskell Platform\8.0.1
 
-SET PATH=%CMAKE_HOME%\bin;%QT_HOME%\bin;%JAVA_HOME%\bin;%MVN_HOME%\bin;%ANT_HOME%\bin;%CYGWIN_HOME%\bin;%GRADLE_HOME%\bin;%NASM_HOME%;%HG_HOME%;%GROOVY_HOME%\bin;%PATH%
+SET PATH=%GIT_HOME%\bin;%CMAKE_HOME%\bin;%QT_HOME%\bin;%JAVA_HOME%\bin;%MVN_HOME%\bin;%ANT_HOME%\bin;%CYGWIN_HOME%\bin;%GRADLE_HOME%\bin;%NASM_HOME%;%HG_HOME%;%GROOVY_HOME%\bin;%HASKELL_HOME%\bin;%PATH%
 CALL "%MSVC_HOME%\vcvarsall.bat"
 
 echo.
@@ -45,7 +47,11 @@ where nasm
 echo.
 where hg
 echo.
+where git
+echo.
 where groovy
+echo.
+where ghci
 echo ==========================================================================
 
 SET prompt=$_[%computername%] $d$s$t$_$p$_$_$+$g$s
