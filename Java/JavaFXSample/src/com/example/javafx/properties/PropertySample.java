8f00b6e9590c3e394d05fe97610bf246f64f8fae
Andreas Fester <Andreas.Fester@gmx.de>
2017-03-10 16:25:05 +0100

    Added reactive examples

--- Java/JavaFXSample/src/com/example/javafx/properties/PropertySample.java
+++ Java/JavaFXSample/src/com/example/javafx/properties/PropertySample.java
@@ -2,11 +2,21 @@ package com.example.javafx.properties;
 
 import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.SimpleIntegerProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ListChangeListener;
+import javafx.collections.ObservableList;
 
 public class PropertySample {
 
     
     public void run() {
+        list.addListener( (ListChangeListener.Change<? extends String> change) -> System.err.println(change));
+
+        simpleSample();
+        listSample();
+    }
+
+    private void simpleSample() {
         System.err.println("Running property sample ...");
         
         IntegerProperty ip = new SimpleIntegerProperty(123);
@@ -26,7 +36,21 @@ public class PropertySample {
         System.err.println("ip:" + ip);
         System.err.println("-------------------------------------------------");
     }
-    
+
+    private ObservableList<String> list = FXCollections.observableArrayList();
+
+    private void listSample() {
+//        list.addListener( (ListChangeListener.Change<? extends String> change) -> System.err.println(change));
+        list.add("Hello");
+        list.add("World");
+        list.remove("xyz");
+        list.remove("Hello");
+        list.addAll("Welcome", "Moon");
+        System.err.println(list);
+        
+        ObservableList<String> list2 = FXCollections.observableArrayList();
+    }
+
     public static void main(String[] args) {
         new PropertySample().run();
     }
