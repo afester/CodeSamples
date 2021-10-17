253f224e3ce707e5925ae013ee3d6d9803c500ef
Andreas Fester <Andreas.Fester@gmx.de>
2021-10-17 21:50:14 +0200

    Simplified configuration for colorized log output

--- Java/ColorizedLog/src/main/java/afester/logging/ColorizedLog.java
+++ Java/ColorizedLog/src/main/java/afester/logging/ColorizedLog.java
@@ -7,12 +7,12 @@ public class ColorizedLog {
 	
     protected static final Logger log = LogManager.getLogger();
 
-	public static void main(String[] args) {
-		log.trace("Trace Message");
-		log.debug("Debug Message");
-		log.info("Info Message");
-		log.warn("Warning Message");
-		log.error("Error Message");
-		log.fatal("Fatal error message");
-	}
+    public static void main(String[] args) {
+        log.trace("Trace Message");
+        log.debug("Debug Message");
+        log.info("Info Message");
+        log.warn("Warning Message");
+        log.error("Error Message");
+        log.fatal("Fatal error message");
+    }
 }
